901f712f3ee73af3f48060808657c88134349c66
nurigaon96 <nurigaon96@gmail.com>
2024-12-06 15:46:26 +0900

    [#36] feat: GlobalExceptionHandler 예외 코드 추가

--- b2c/src/main/java/com/sparta/b2c/B2cApplication.java
+++ b2c/src/main/java/com/sparta/b2c/B2cApplication.java
@@ -1,12 +1,10 @@
 package com.sparta.b2c;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.sparta", exclude = { SecurityAutoConfiguration.class })
 public class B2cApplication {
 
     public static void main(String[] args) {
