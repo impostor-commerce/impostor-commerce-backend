1c85739998c0ba03ccb24a0c0e1aaa4cdd49a28c
hwk9407 <hwk__@naver.com>
2024-12-06 11:36:24 +0900

    [#20]refactor: Config 패키지 경로 리팩토링

--- b2c/src/main/java/com/sparta/b2c/config/JpaConfig.java
+++ b2c/src/main/java/com/sparta/b2c/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.sparta.b2c.product.config;
+package com.sparta.b2c.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
