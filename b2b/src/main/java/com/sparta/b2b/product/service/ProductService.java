d1fe54cdb48555ad68b2939556a820f78cacc99b
gebeen-dev <rrdd1207@gmail.com>
2024-12-08 23:21:16 +0900

    [#8]feat: B2Bmember 상픔등록 권한 추가
    APPROVED된 B2Bmember만 등록 할 수 있다.

--- b2b/src/main/java/com/sparta/b2b/product/service/ProductService.java
+++ b2b/src/main/java/com/sparta/b2b/product/service/ProductService.java
@@ -2,19 +2,33 @@ package com.sparta.b2b.product.service;
 
 import com.sparta.b2b.product.dto.request.ProductCreateRequest;
 import com.sparta.b2b.product.dto.response.ProductCreateResponse;
+import com.sparta.impostor.commerce.backend.domain.b2bMember.entity.B2BMember;
+import com.sparta.impostor.commerce.backend.domain.b2bMember.enums.ApproveStatus;
+import com.sparta.impostor.commerce.backend.domain.b2bMember.repository.B2BMemberRepository;
 import com.sparta.impostor.commerce.backend.domain.product.entity.Product;
-import com.sparta.impostor.commerce.backend.domain.product.enums.Category;
 import com.sparta.impostor.commerce.backend.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.sparta.impostor.commerce.backend.domain.product.entity.QProduct.product;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ProductService {
 
 	private final ProductRepository productRepository;
+	private final B2BMemberRepository b2bMemberRepository;
 
-	public ProductCreateResponse createProduct(String email, ProductCreateRequest request) {
+	public ProductCreateResponse createProduct(Long id, ProductCreateRequest request) {
+
+		B2BMember member = b2bMemberRepository.findById(1l)
+			.orElseThrow(() -> new IllegalArgumentException("해당 ID를 가진 멤버가 존재하지 않습니다."));
+
+		if (member.getApproveStatus() != ApproveStatus.APPROVED) {
+			throw new IllegalStateException("승인된 멤버만 상품을 등록할 수 있습니다.");
+		}
 
 		Product product = productRepository.save(request.toEntity());
 
