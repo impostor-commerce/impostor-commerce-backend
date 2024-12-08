4e2bdb7035b21139fa0a522a78256a964fb38d8d
gebeen-dev <rrdd1207@gmail.com>
2024-12-08 21:20:39 +0900

    [#8]feat: createProductService 구현

--- b2b/src/main/java/com/sparta/b2b/product/service/ProductService.java
+++ b2b/src/main/java/com/sparta/b2b/product/service/ProductService.java
@@ -1,13 +1,23 @@
 package com.sparta.b2b.product.service;
 
-import com.sparta.b2b.product.dto.response.ProductResponse;
+import com.sparta.b2b.product.dto.request.ProductCreateRequest;
+import com.sparta.b2b.product.dto.response.ProductCreateResponse;
+import com.sparta.impostor.commerce.backend.domain.product.entity.Product;
+import com.sparta.impostor.commerce.backend.domain.product.enums.Category;
+import com.sparta.impostor.commerce.backend.domain.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
-	public ProductResponse createProduct() {
-		return null;
+
+	private final ProductRepository productRepository;
+
+	public ProductCreateResponse createProduct(String email, ProductCreateRequest request) {
+
+		Product product = productRepository.save(request.toEntity());
+
+		return ProductCreateResponse.from(product);
 	}
 }
